916edea9f6795eb612965e337c3804b08a5f3039
Jon Fox <jonfuchs45@gmail.com>
2019-04-08 12:45:14 -0600

    initialized building1 and started planning out the elevator class

--- elevator-simulation.js
+++ elevator-simulation.js
@@ -15,3 +15,24 @@ class Building{
         this.elevators = elevators;
     }
 }
+
+// 1. Initialize the elevator simulation with the desired number of elevators, and the desired number of floors. Assume ground/min of 1.
+
+// I am initilizing the sim with building1, it has 3 floors and two elevators to start with. 
+
+const building1 = new Building('hotel', 3, 2);
+
+
+/* Now I'm going to create an Elevator class. 
+each Elevator will have the following properties:
+- name (this will be a string)
+- currentFloor (this will be a number)
+- inMotion (this will be a boolean)
+- doorsOpen (this will be a boolean)
+- doorsClosed (this will be a boolean)
+- elevators this will be a number)
+
+Each elevator will have the following methods: 
+
+
+*/
