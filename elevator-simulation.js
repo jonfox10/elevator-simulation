1243ce51bf4e40ff7ede463043e06e37532913cd
Jon Fox <jonfuchs45@gmail.com>
2019-04-08 14:11:32 -0600

    ran out of time, added the plans for more needed methods in the elevator class

--- elevator-simulation.js
+++ elevator-simulation.js
@@ -30,7 +30,8 @@ const building1 = new Building('hotel', 3, 2, ['el1', 'el2']);
 /* Now I'm going to create an Elevator class. 
 each Elevator will have the following properties:
 - name (this will be a string)
-- highestFloor (number to set limit on floors the elevato can go)
+- topFloor (number to set limit on floors the elevato can go)
+- groundFloor (numeber that equals 1)
 - queue (this will be an array of all the current requests)
 - currentFloor (this will be a number)
 - inMotion (this will be a boolean)
@@ -38,8 +39,9 @@ each Elevator will have the following properties:
 - doorsClosed (this will be a boolean)
 - goinUp (this will be a boolean)
 - goinDown (this will be a boolean)
--
-
+- occupied (this will be a boolean)
+- trips (a number that )
+- mainteneceMode (this will be a boolean)
 
 Each elevator will have the following methods: 
     -request
@@ -47,14 +49,22 @@ Each elevator will have the following methods:
     -movingReport 
         - updates currentFloor property as it moves and changes locations. 
     -closeDoors
-
+        -closes the doors
     -openDoors
+        - opens the elevator doors
+    -startTrip
+        - this will set the elevator into motion to its requested destination
+    -tripCount
+        - this method will add 1 to the number of trips after every trip, once trips equals 100, it will set MaintenenceMode to true and shut of functionality.
+    -maintenence
+        - this method will reset the trips amount to 0
 */
 
 class Elevator{
-    constructor(name, highestFloor){
+    constructor(name, topFloor){
         this.name = name;
-        this.highestFloor = highestFloor;
+        this.topFloor = topFloor;
+        this.groundFloor = 1;
         this.queue = []
         this.currentFloor = 1;
         this.inMotion = false;
@@ -63,27 +73,36 @@ class Elevator{
         this.goingUp = false;
         this.goingDown = false; 
         this.movingTo = 0;
+        this.occupied = false;
+        this.trips = 0;
     }
 
-    request(floor){
+    request(currentFloor, floorRequestedTo){
         //first we will check if the request floor is the same as current floor of the elevator.
         //*** Actually I think we should push it first and then check. */
         // if(floor === this.currentFloor){}
 
         // when someone pushes button outside of an elevator, their floor number will get pushed into the back of the queue array. Im going to use a first in first out approach so that the people who have been waiting the longest get the elevator sooner. 
         // I will adress the case of the elevator stopping for others that are also on the same route, but not first in the queue later.  
-        this.queue.push(floor);
+        this.queue.push(currentFloor);
         
-        //lets check to see if the elevator is already heading somewhere and is in motion. If not we will send it to queue[0]
-        if(this.movingTo === 0 & !this.inMotion) {
+        //lets check to see if the elevator is occupied first
+        if(!this.occupied) {
             
-
         }
+        //lets check to see if the elevator is already heading somewhere and is in motion. If not we will send it to queue[0]
+        
 
 
 
     }
 
+    // this method is incomplete but the intent is to call this in the request method to put the elevator into motion to the new floor.  
+    startTrip(floor){
+        if(this.doorsClosed){
+            this.inMotion = true;
+        }
+    }
 
     //method to close the doors of the elevator
     closeDoors(){
