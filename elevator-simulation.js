58e209bba43f0b6bd499fb6fdffba3bcba6ff88e
Jon Fox <jonfuchs45@gmail.com>
2019-04-08 12:31:16 -0600

    first commit starting to design the basic structure of the software

---
+++ elevator-simulation.js
@@ -0,0 +1,17 @@
+
+/* I'm going to start out by making a class called Building. 
+each Building will have the following properties:
+- name (this will be a string)
+- floors (this will be a number)
+- elevators this will be a number)
+
+Each Building will have the following methods: 
+
+*/
+class Building{
+    constructor(name, floors, elevators){
+        this.name = name;
+        this.floors = floors;
+        this.elevators = elevators;
+    }
+}
